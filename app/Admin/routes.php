b2805287cd406106b6fe23ecfa624a3646600ecf
wangbaojie <wangbaojie1@qq.com>
2020-08-05 00:05:56 +0800

    123

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,7 +11,7 @@ Route::group([
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
 
-   // $router->get('/', 'HomeController@index')->name('home');
+    $router->get('/', 'HomeController@index')->name('home');
     $router->resource('/', DataController::class);
 
 
